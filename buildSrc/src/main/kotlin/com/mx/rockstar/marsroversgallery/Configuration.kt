04996e34bea7fc9412be3411485ddb0e89d17506
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2022-11-14 23:27:01 -0600

    chore: add sonarQube support

--- buildSrc/src/main/kotlin/com/mx/rockstar/marsroversgallery/Configuration.kt
+++ buildSrc/src/main/kotlin/com/mx/rockstar/marsroversgallery/Configuration.kt
@@ -4,9 +4,9 @@ object Configuration {
     const val compileSdk = 33
     const val targetSdk = 33
     const val minSdk = 23
-    const val majorVersion = 1
-    const val minorVersion = 1
-    const val patchVersion = 0
+    const val majorVersion = 0
+    const val minorVersion = 0
+    const val patchVersion = 1
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val versionCode = 1
 }
